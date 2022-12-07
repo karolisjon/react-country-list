9717478337a5656528c2c103e5d7872df72d4f8d
karolisjon <karolisjonn@gmail.com>
2022-12-07 19:46:42 +0200

    feat: implemented functionality to render countries from the state variable

--- src/components/country-list.jsx
+++ src/components/country-list.jsx
@@ -1,25 +1,36 @@
 import * as React from 'react';
-import { Box, Container } from '@mui/material'
+import { Box, Container, Typography } from '@mui/material'
 
 const CountryList = () => {
-  const [country, setCountry] = React.useState([]);
+  const [countries, setCountries] = React.useState([]);
 
-const fetchAll = async () => {
-  const response = await fetch('https://restcountries.com/v2/all?fields=name,region,area');
-  const data = await response.json();
+  const fetchAll = async () => {
+    const response = await fetch('https://restcountries.com/v2/all?fields=name,region,area');
+    const data = await response.json();
 
-  console.table(data);
-  setCountry(data);
-}
+    setCountries(data);
+  }
 
-React.useEffect(() => {
-  fetchAll();
-}, []);
+  React.useEffect(() => {
+    fetchAll();
+  }, []);
 
   return (
     <Container maxWidth='lg'>
       <Box>
-        <pre>{JSON.stringify(country, null, 4)}</pre>
+        {countries.map(({ name, region, area, independent}) => (
+          <Box sx={{
+            my: 2,
+            p: 2,
+            backgroundColor: 'rgb(238 234 251)',
+            borderRadius: 1
+          }}>
+            <Typography>Name: {name}</Typography>
+            <Typography>Region: {region}</Typography>
+            <Typography>Area: {area}</Typography>
+            <Typography>Independent: {independent}</Typography>
+          </Box>
+        ))}
       </Box>
     </Container >
   )
